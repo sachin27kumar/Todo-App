7907bb6e6a27f777aec086a7e08ec2382d95af79
Sachin Sheoran <sachin27sheoran@gmail.com>
2023-04-27 14:02:43 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const addbtn = document.getElementById("todoInput");
+const todoInput = document.getElementById("todoInput");
+const todoList = document.getElementById("todoList");
+
+function taskAdd(){
+    const todoText = todoInput.value;
+    const listItem = document.createElement("li");
+    listItem.innerHTML=todoText;
+    todoList.appendChild(listItem);
+    todoInput.value = "";
+};
